534fbd921893b38fc7d5963ef2b8b210a4037229
amir haim <amirbiron@gmail.com>
2025-08-31 14:37:26 +0300

    Upload App.py via Telegram bot

---
+++ src/App.py
@@ -0,0 +1,4 @@
+flake8 | ❌ נכשל — ./myy.py:1:11: E999 SyntaxError: invalid character '❌' (U+274C)
+mypy   | ❌ נכשל — myy.py:1: error: invalid character '❌' (U+274C)  [syntax]
+bandit | ❌ נכשל — Traceback (most recent call last):
+black  | ❌ נכשל — error: cannot format /tmp/repo_val_hz2ld96d/repo/amirbiron-Api-Finder-5c8582b/myy.py: '❌'
