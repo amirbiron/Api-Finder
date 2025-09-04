65651d2ecc0d850713c7d458177e9ee77313e66e
amir haim <amirbiron@gmail.com>
2025-09-04 10:30:52 +0300

    fix: move permissions to job level

--- myy.py
+++
@@ -1,4 +0,0 @@
-flake8 | ❌ נכשל — ./activity_reporter.py:7:1: E302 expected 2 blank lines, found 1
-mypy   | ❌ נכשל — i18n/strings_he.py: error: Source file found twice under different module names: "strings_he" and "i18n.strings_he"
-bandit | ❌ נכשל — Traceback (most recent call last):
-black  | ❌ נכשל — would reformat /tmp/repo_val_wgsfkvsf/repo/amirbiron-CodeBot-26a70aa/database/init.py
